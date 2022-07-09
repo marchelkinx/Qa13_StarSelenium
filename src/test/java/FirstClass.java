40d45ee563b8885b4a3be92c4eff5ec2591f0732
marchelkinx <marchelkinx@gmail.com>
2022-07-09 12:38:04 +0300

    WebDriver

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,2 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class FirstClass {
+    WebDriver wd;
+
+    @Test
+    public void  preCondition(){
+        WebDriverManager.chromedriver().setup();
+        wd = new ChromeDriver();
+//        wd.get("");
+        wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/home");
+//        wd.navigate().forward();
+//        wd.navigate().back();
+//        wd.navigate().refresh();
+    }
+
 }
